ddf0c435834901ee780334b4423e4753a14980c2
edwardlp <differ-lp@mail.ru>
2016-08-25 00:11:32 +0300

    nerds

---
+++ nerds/js/script.js
@@ -0,0 +1,15 @@
+jQuery('document').ready(function() {
+
+  var number;
+
+  number = 111;
+
+  if (number != 111) {
+    alert('Хорошо');
+  } else if (number == 111) {
+    alert('number равен 111');
+  } else {
+    alert('Это не 111');
+  }
+
+});
